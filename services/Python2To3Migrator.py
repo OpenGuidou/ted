99983a8a72779c7ee1b8e129da2c4e838fda98fc
Guillaume Doussin <guillaume.doussin@amadeus.com>
2024-06-04 12:25:44 +0200

    Add file migration

--- services/Python2To3Migrator.py
+++ services/Python2To3Migrator.py
@@ -12,16 +12,22 @@ class Python2To3Migrator(TEDGenerator):
 
     def runGeneration(self, retriever, llm, output_parser) -> None:
         template = """You are a skilled python developer. Your job is to migrate python 2 projects to python 3.
-        You read the code provided and provide a list of files that needs to be modified during the migration.
-        You return the answer without any explanation in a Json format.
+        You read the code provided and either provide a list of files that needs to be modified during the migration or the full content of the file requested after the migration.
+        The generated code should work and should not summarize the changes but provide the full content of the file.
+        You return the answer without any explanation in a Json format for the files listing case, and as a text file for the file migration case.
 
-        Here is an example of the expected output:
+        Here is an example of the expected output for the file listing case:
         {{
             "files": [
                 "file1.py",
                 "file2.py"
             ]
         }}
+
+        Here is an example of the expected output for the file migration case:
+        ```migrated
+        # Migrated file content
+        ```
                 
         Here are the files that need to be taken into account:
         {context}
@@ -50,8 +56,19 @@ class Python2To3Migrator(TEDGenerator):
             files = json.loads(filesAnswer)['files']
 
             for file in files:
+                print("-------------------------------------------------\n")
                 print("File to migrate: {}".format(file))
+                fileAnswer = chain.invoke("Give me the migrated version of the file {}, with the full content of the file".format(file))
+                print(fileAnswer)
 
+                fileParsed = re.search('```migrated\n([\w\W]*?)\n```', fileAnswer)
+                if fileParsed is not None:
+                    migratedContent = fileParsed.group(1)
+                    f = open("clone/" + file, "w")
+                    f.write(migratedContent)
+                    f.close()
+                else:
+                    print("File parsing failure")
         else:
             print("Answer parsing failure")
     
