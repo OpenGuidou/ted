e6a1dee083f3cba146f721cf81d176d09b81396c
pguimon <pierre.guimon@amadeus.com>
2024-06-06 10:11:13 +0200

    Add tests for GitHelper.py class

--- tests/helpers/test_GitHelper.py
+++ tests/helpers/test_GitHelper.py
@@ -1,7 +1,12 @@
+import os
 import unittest
+
+from git import Repo
 import requests
+import shutil
 from helpers.GitHelper import GitHelper
 from unittest import mock
+
 
 # -------------------------------------------------------------------------------------------
 # Mocks used in the below unit test class
@@ -11,6 +16,47 @@ def mocked_pull_request_failure(*args, **kwargs):
     Mock function to mimic a failure to create a pull request
     """
     raise requests.exceptions.HTTPError
+
+def mocked_pull_request_success(*args, **kwargs):
+    """
+    Mock function to mimic a success to create a pull request
+    """
+    response = requests.Response()
+    response.status_code = 200
+
+    return response
+
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
+
+def mocked_git_repository_clone(*args, **kwargs):
+    shutil.rmtree(args[0], onerror=onerror)
+    os.makedirs(args[0])
+
+    with open(os.path.join(args[0], "test_file.txt"), "w") as file:
+        file.write("Test text goes here")
+
+    r = Repo.init(args[0])
+    r.git.add(all=True)
+    r.remote = mock.MagicMock()
+
+    return r
 
 # -------------------------------------------------------------------------------------------
 # Unit test class
@@ -30,6 +76,52 @@ class TestGitHelper(unittest.TestCase):
         self.assertRaises(requests.exceptions.HTTPError,
                           lambda: git_helper.create_pull_request(repository, title, description, head_branch,
                                                                  base_branch, git_token))
+        expected_headers = {
+            "Authorization": "token git_token",
+            "Content-Type": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+        self.assertIn(mock.call("https://api.github.com/repos/repository/pulls",
+                                headers=expected_headers,
+                                data="{\"title\": \"title\", \"body\": \"description\", "
+                                     "\"head\": \"head_branch\", \"base\": \"base_branch\"}"),
+                      mock_requests_post.call_args_list)
+
+    # Mock some methods. The mock object is passed in to our test case method.
+    @mock.patch('helpers.GitHelper.requests.post', side_effect=mocked_pull_request_success)
+    def test_create_pull_request_ok(self, mock_requests_post):
+        git_helper = GitHelper()
+        repository = "repository"
+        title = "title"
+        description = "description"
+        head_branch = "head_branch"
+        base_branch = "base_branch"
+        git_token = "git_token"
+        git_helper.create_pull_request(repository, title, description, head_branch, base_branch, git_token)
+        expected_headers = {
+            "Authorization": "token git_token",
+            "Content-Type": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+        self.assertIn(mock.call("https://api.github.com/repos/repository/pulls",
+                                headers=expected_headers,
+                                data="{\"title\": \"title\", \"body\": \"description\", "
+                                     "\"head\": \"head_branch\", \"base\": \"base_branch\"}"),
+                      mock_requests_post.call_args_list)
+
+    # Mock some methods. The mock object is passed in to our test case method.
+    @mock.patch('helpers.GitHelper.Repo', side_effect=mocked_git_repository_clone)
+    def test_push_changes_in_pull_request(self, mock_git_repo):
+        git_helper = GitHelper()
+        repository = "repository"
+        message = "message"
+        target_branch = "target_branch"
+        head_branch = "head_branch"
+        git_token = "git_token"
+        clone_path = "clone_path"
+        git_helper.push_changes_in_pull_request(repository, message, target_branch, head_branch, git_token, clone_path)
+
+        self.assertIn(mock.call(clone_path), mock_git_repo.call_args_list)
 
 # -------------------------------------------------------------------------------------------
 # Main
